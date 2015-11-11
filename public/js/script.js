bb5b4645999d8478b25f0fa79dd68f2f7aaa99e5
Robert Brown <reb.pdx@gmail.com>
2015-11-11 15:48:20 -0800

    Trying more stuff...

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@
 // Change this to go to next data collecting page when ready
 function check(form) {
     if(form.user.value == "admin" && form.pass.value == "admin") {
-        window.location.href = "./views/graph.jade";
+        script(type="text/javascript" src="/js/d3stuff.js")
     } else {
         alert("Error Password or Username");
     }
