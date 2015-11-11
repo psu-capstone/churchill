00ef713d6a29264334c960c9b043696ebec6cbc8
ScienceRobot <ryanandersont@gmail.com>
2015-11-11 11:43:47 -0800

    travis, gulp file additions, cleanups

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('public/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('public/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('build'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build'));
+});
+
+// Default Task
+gulp.task('default', ['lint', 'scripts']);
---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ryan on 11/10/15.
+ */
+
+// Change this to go to next data collecting page when ready
+function check(form) {
+    if(form.user.value == "admin" && form.pass.value == "admin") {
+        alert("Good Login, this will then go to the next page.... :]");
+    } else {
+        alert("Error Password or Username");
+    }
+}
