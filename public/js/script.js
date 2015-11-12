7b7382614578cbddc4dadd0664e3da83feade839
ScienceRobot <ryanandersont@gmail.com>
2015-11-11 16:11:49 -0800

    checking webservice changes

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@
 // Change this to go to next data collecting page when ready
 function check(form) {
     if(form.user.value == "admin" && form.pass.value == "admin") {
-        alert("Good Login, this will then go to the next page.... :]");
+        alert("Good login, this will then go to the next page.... :D");
     } else {
         alert("Error Password or Username");
     }
