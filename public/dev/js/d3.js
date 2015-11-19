bd9f3345223815e44d589e50a25f53c41dc25930
ScienceRobot <ryanandersont@gmail.com>
2015-11-18 16:43:12 -0800

    d3 stuff

--- public/dev/js/d3.js
+++ public/dev/js/d3.js
@@ -2,3 +2,23 @@
  *
  * Created by ryan on 11/18/15.
  */
+angular.module('d3Test', []).
+
+    directive('bars', function ($parse) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div id="chart"></div>',
+            link: function (scope, element, attrs) {
+                var data = attrs.data.split(','),
+                    chart = d3.select('#chart')
+                        .append("div").attr("class", "chart")
+                        .selectAll('div')
+                        .data(data).enter()
+                        .append("div")
+                        .transition().ease("elastic")
+                        .style("width", function(d) { return d + "%"; })
+                        .text(function(d) { return d + "%"; });
+            }
+        };
+    });
