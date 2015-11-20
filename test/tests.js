9b05a33171b622a1a61b1ab2e5918e2c9be3f994
ScienceRobot <ryanandersont@gmail.com>
2015-11-20 15:50:12 -0800

    more tests to account for rest of current app

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,34 @@
 /**
+ * Karma tests
  *
  * Created by ryan on 11/16/15.
  */
+
+describe('Unit: Routing', function () {
+    it('should map routes to controllers', function() {
+        module('democracy-lab-app');
+
+        inject(function($route) {
+
+            expect($route.routes['/'].controller).toBe('main-controller');
+            expect($route.routes['/'].templateUrl).
+                toEqual('pages/default.html');
+
+            expect($route.routes['/home'].controller).toBe('main-controller');
+            expect($route.routes['/home'].templateUrl).
+                toEqual('pages/default.html');
+
+            expect($route.routes['/login'].controller).toBe('login-controller');
+            expect($route.routes['/login'].templateUrl).
+                toEqual('pages/login.html');
+
+            expect($route.routes['/explore'].controller).toBe('explore-controller');
+            expect($route.routes['/explore'].templateUrl).
+                toEqual('pages/d3graph.html');
+            expect($route.routes['/explore'].directive).toBe('bars');
+        });
+    });
+});
 
 describe('Unit: main-controller', function() {
     // Load the module with MainController
@@ -52,5 +79,47 @@ describe('Unit: login-controller', function() {
         scope.username = "admin";
         scope.authenticate();
         expect(scope.authorized).toEqual(true);
+        });
+});
+
+describe('Unit: issue-controller', function($scope) {
+    // Load the module with MainController
+    beforeEach(module('democracy-lab-app'));
+
+    var ctrl, scope;
+    // inject the $controller and $rootScope services
+    // in the beforeEach block
+    beforeEach(inject(function($controller, $rootScope) {
+        // Create a new scope that's a child of the $rootScope
+        scope = $rootScope.$new();
+        // Create the controller
+        ctrl = $controller('issue-controller', {
+            $scope: scope
+        });
+    }));
+    it('should verify title is "Weigh in on..."',
+        function() {
+            expect(scope.title).toEqual("Weigh in on an issue");
+        });
+});
+
+describe('Unit: explore-controller', function($scope) {
+    // Load the module with MainController
+    beforeEach(module('democracy-lab-app'));
+
+    var ctrl, scope;
+    // inject the $controller and $rootScope services
+    // in the beforeEach block
+    beforeEach(inject(function($controller, $rootScope) {
+        // Create a new scope that's a child of the $rootScope
+        scope = $rootScope.$new();
+        // Create the controller
+        ctrl = $controller('explore-controller', {
+            $scope: scope
+        });
+    }));
+    it('should verify title is "Explore the..."',
+        function() {
+            expect(scope.title).toEqual("Explore the issues");
         });
 });
