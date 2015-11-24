67549852e1ad4fd7fc19380d98a442c5e910c581
m-d-s <mds6@pdx.edu>
2015-11-23 17:10:17 -0800

    reaping karma

--- test/tests.js
+++ test/tests.js
@@ -34,20 +34,16 @@ describe('Unit: main-controller', function() {
     // Load the module with MainController
     beforeEach(module('democracy-lab-app'));
 
-    var ctrl, scope;
+    var ctrl;
     // inject the $controller and $rootScope services
     // in the beforeEach block
-    beforeEach(inject(function($controller, $rootScope) {
-        // Create a new scope that's a child of the $rootScope
-        scope = $rootScope.$new();
+    beforeEach(inject(function($controller) {
         // Create the controller
-        ctrl = $controller('main-controller', {
-            $scope: scope
-        });
+        ctrl = $controller('main-controller');
     }));
     it('should verify title is "Home"',
         function() {
-            expect(scope.title).toEqual("Home");
+            expect(ctrl.title).toEqual("Home");
     });
 });
 
@@ -55,71 +51,59 @@ describe('Unit: login-controller', function() {
     // Load the module with MainController
     beforeEach(module('democracy-lab-app'));
 
-    var ctrl, scope;
+    var ctrl;
     // inject the $controller and $rootScope services
     // in the beforeEach block
-    beforeEach(inject(function($controller, $rootScope) {
-        // Create a new scope that's a child of the $rootScope
-        scope = $rootScope.$new();
+    beforeEach(inject(function($controller) {
         // Create the controller
-        ctrl = $controller('login-controller', {
-            $scope: scope
-        });
+        ctrl = $controller('login-controller');
     }));
 
     it('Should verify title is "Login or Create Account"', function() {
-        expect(scope.title).toEqual("Login or Create Account");
+        expect(ctrl.title).toEqual("Login or Create Account");
         });
 
     it('Verify initialized false', function() {
-        expect(scope.authorized).toEqual(false)
+        expect(ctrl.authorized).toEqual(false)
         });
 
     it('Verify correct login', function () {
-        scope.username = "admin";
-        scope.authenticate();
-        expect(scope.authorized).toEqual(true);
+        ctrl.username = "admin";
+        ctrl.authenticate();
+        expect(ctrl.authorized).toEqual(true);
         });
 });
 
-describe('Unit: issue-controller', function($scope) {
+describe('Unit: issue-controller', function() {
     // Load the module with MainController
     beforeEach(module('democracy-lab-app'));
 
-    var ctrl, scope;
+    var ctrl;
     // inject the $controller and $rootScope services
     // in the beforeEach block
-    beforeEach(inject(function($controller, $rootScope) {
-        // Create a new scope that's a child of the $rootScope
-        scope = $rootScope.$new();
+    beforeEach(inject(function($controller) {
         // Create the controller
-        ctrl = $controller('issue-controller', {
-            $scope: scope
-        });
+        ctrl = $controller('issue-controller');
     }));
     it('should verify title is "Weigh in on..."',
         function() {
-            expect(scope.title).toEqual("Weigh in on an issue");
+            expect(ctrl.title).toEqual("Weigh in on an issue");
         });
 });
 
-describe('Unit: explore-controller', function($scope) {
+describe('Unit: explore-controller', function() {
     // Load the module with MainController
     beforeEach(module('democracy-lab-app'));
 
-    var ctrl, scope;
+    var ctrl;
     // inject the $controller and $rootScope services
     // in the beforeEach block
-    beforeEach(inject(function($controller, $rootScope) {
-        // Create a new scope that's a child of the $rootScope
-        scope = $rootScope.$new();
+    beforeEach(inject(function($controller) {
         // Create the controller
-        ctrl = $controller('explore-controller', {
-            $scope: scope
-        });
+        ctrl = $controller('explore-controller');
     }));
     it('should verify title is "Explore the..."',
         function() {
-            expect(scope.title).toEqual("Explore the issues");
+            expect(ctrl.title).toEqual("Explore the issues");
         });
 });
