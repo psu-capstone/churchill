40488e0bc01dcdf54e35ed403fd0d3f8d78e1121
m-d-s <mds6@pdx.edu>
2015-12-18 13:06:45 -0800

    readlink broken for Mac, substituted backtick perl command

--- api/setup.sh
+++ api/setup.sh
@@ -2,9 +2,8 @@
 set -e
 
 # path of script
-# use readlink if Ubuntu
-# greadlink on Mac
-SCRIPT=$(greadlink -f $0)
+# cludged universal perl command, don't worry about it
+SCRIPT=`perl -e 'use Cwd "abs_path";print abs_path(shift)' $0`
 # script directory
 DIR=`dirname $SCRIPT`
 
