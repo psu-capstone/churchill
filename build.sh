14a0f009d457532b40fce4fecfb694c4320fe37a
m-d-s <mds6@pdx.edu>
2016-02-21 12:29:39 -0800

    removed old build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Gulp Files"
-./node_modules/.bin/gulp
-echo "Building bundle.js"
-./node_modules/.bin/gulp package
-echo "Browserify"
-./node_modules/.bin/gulp browserify
-echo "Start localhost"
-./node_modules/.bin/gulp connect
